565a5031924d0bb332e8e24db7c5affb038b8b54
rorschachwilpeng <48847996+rorschachwilpeng@users.noreply.github.com>
2026-07-11 16:40:11 +0800

    build: support Swift 5.9 toolchains

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
@@ -10,5 +10,5 @@ let package = Package(
     targets: [
         .executableTarget(name: "CodexUsageMenuBar")
     ],
-    swiftLanguageModes: [.v5]
+    swiftLanguageVersions: [.v5]
 )
